c2dd3c2ad834fdbd4d3c5aa56fa84b78dcd964d8
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2017-09-21 17:21:44 -0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'mwapi',
+  packages = ['mwapi'],
+  version = '0.1',
+  description = 'A simple and modern library for interacting with the MediaWiki API.',
+  author = 'Guilherme Gonçalves',
+  url = 'https://github.com/eggpi/mwapi', # use the URL to the github repo
+  download_url = 'https://github.com/eggpi/mwapi/archive/0.1.tar.gz',
+  keywords = ['wikipedia', 'mediawiki'],
+  classifiers = [],
+)
