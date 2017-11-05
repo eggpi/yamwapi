66090b8529c8750e6d3a653a15a99d0e41d64684
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2017-11-05 22:44:29 +0000

    0.2

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
   name = 'yamwapi',
   packages = ['yamwapi'],
-  version = '0.1',
+  version = '0.2',
   description = 'A simple and modern library for interacting with the MediaWiki API. Use me to talk to Wikipedia.',
   author = 'Guilherme Gonçalves',
   author_email = 'guilherme.p.gonc@gmail.com',
   url = 'https://github.com/eggpi/yamwapi', # use the URL to the github repo
-  download_url = 'https://github.com/eggpi/yamwapi/archive/0.1.tar.gz',
+  download_url = 'https://github.com/eggpi/yamwapi/archive/0.2.tar.gz',
   keywords = 'wikipedia mediawiki',
   classifiers = [],
 )
