8a03ff8fe0e9578d444302b1c3b15695b9b4589b
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2019-06-30 19:53:01 +0100

    0.3

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 # -*- encoding: utf-8 -*-
 
-from distutils.core import setup
-setup(
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
   name = 'yamwapi',
   packages = ['yamwapi'],
-  version = '0.2',
+  version = '0.3',
   description = 'A simple and modern library for interacting with the MediaWiki API. Use me to talk to Wikipedia.',
   author = 'Guilherme Gonçalves',
   author_email = 'guilherme.p.gonc@gmail.com',
   url = 'https://github.com/eggpi/yamwapi', # use the URL to the github repo
-  download_url = 'https://github.com/eggpi/yamwapi/archive/0.2.tar.gz',
+  download_url = 'https://github.com/eggpi/yamwapi/archive/0.3.tar.gz',
   keywords = 'wikipedia mediawiki',
   classifiers = [],
 )
